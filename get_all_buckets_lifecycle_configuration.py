14dde27124e0b4453a096d968cd9e20d62a02ce5
barsilver <45267926+barsilver@users.noreply.github.com>
2022-09-06 14:27:10 +0300

    Add files via upload

---
+++ get_all_buckets_lifecycle_configuration.py
@@ -0,0 +1,14 @@
+#!/Users/bar.silver/.pyenv/shims/python3
+
+import boto3
+from botocore.exceptions import ClientError
+
+
+s3 = boto3.resource('s3')
+
+for bucket in s3.buckets.all():
+    try:
+        rules = bucket.Lifecycle().rules
+    except:
+        rules = 'No Policy'
+    print(bucket.name, rules)
