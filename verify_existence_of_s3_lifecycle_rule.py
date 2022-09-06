917de9c5302e91bb1812408da3580911a0055355
barsilver <45267926+barsilver@users.noreply.github.com>
2022-09-06 16:02:07 +0300

    name 'bucket' is not defined

--- verify_existence_of_s3_lifecycle_rule.py
+++ verify_existence_of_s3_lifecycle_rule.py
@@ -4,10 +4,10 @@ import boto3
 s3 = boto3.resource('s3')
 buckets = s3.buckets.all() # or buckects list as you wish
 
-for i in buckets:
+for bucket in buckets:
     try:
     # if using buckets list and not s3.buckets.all()
-    #bucket = s3.Bucket(i)
+    #bucket = s3.Bucket(bucket)
         rules = bucket.Lifecycle().rules
         if any(d['ID'] == '<RULE_ID>' for d in rules):
           rules = "Policy Exists"
