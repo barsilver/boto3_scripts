dd471b3a02245731b4d43939dfecf338abd241c1
barsilver <45267926+barsilver@users.noreply.github.com>
2022-09-07 10:32:39 +0300

    Update put_INTELLIGENT_TIERING_lifecycle_configuration.py
    Put INTELLIGENT_TIERING rule named S3_storageClass (rule ID) on each bucket from a file that doesn't contain an expected string. 
    For example, using a source file with bucket names and the strings "No Policy", "Policy Exists" (the S3_storageClass policy in our case), or different rules that the policy doesn't exist among them. Then, the script removes the lines with the "Policy Exists" string and applies the lifecycle configuration rule to the others.

--- put_INTELLIGENT_TIERING_lifecycle_configuration.py
+++ put_INTELLIGENT_TIERING_lifecycle_configuration.py
@@ -1,6 +1,15 @@
-# Edit PROFILE_NAME before running the script.
+# Edit PROFILE_NAME, STRING, FILE  before running the script.
 
 import boto3
+
+strings = ['<STRING>']
+source_file = "<FILE>"
+
+lines = []
+with open(source_file) as file:
+    for line in file:
+        if not any(string in line for string in strings):
+            lines.append(line.split(" ", 1)[0])
 
 lifecycle_configuration = {
     "Rules": [{"ID": "S3_storageClass",
@@ -10,13 +19,14 @@ lifecycle_configuration = {
                "NoncurrentVersionTransitions": [{"NoncurrentDays": 0,
                                                  "StorageClass": "INTELLIGENT_TIERING"}]
                }]
-session = boto3.session.Session(profile_name=<PROFILE_NAME>)  # profile defined in ~/.aws/credentials
+}
 
-for bucket in s3.buckets.all():
-    try:
-        rules = bucket.Lifecycle().rules
-    except:
-        rules = 'INTELLIGENT_TIERING rule added'
-        s3_client = session.client("s3")
-        s3_client.put_bucket_lifecycle_configuration(Bucket=bucket, LifecycleConfiguration=lifecycle_configuration)
-    print(bucket.name, rules)
+
+session = boto3.session.Session(profile_name='<PROFILE_NAME>')  # profile defined in ~/.aws/credentials
+s3 = boto3.resource('s3')
+
+for bucket in lines:
+    message = 'INTELLIGENT_TIERING rule added'
+    s3_client = session.client('s3')
+    s3_client.put_bucket_lifecycle_configuration(Bucket=bucket, LifecycleConfiguration=lifecycle_configuration)
+    print(bucket, message)
